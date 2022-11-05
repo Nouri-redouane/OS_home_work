deccbce0ddf6737b6739b608a4afed3a1cfa159d
karimPrograms <78750964+karimPrograms@users.noreply.github.com>
2022-11-05 08:35:41 -0400

    Update sprite.py

--- sprite.py
+++ sprite.py
@@ -15,14 +15,10 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(midbottom=(80, 300))
         self.gravity = 0
 
-        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
-        self.jump_sound.set_volume(0.2)
-
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
             self.gravity = -20
-            self.jump_sound.play()
 
     def apply_gravity(self):
         self.gravity += 1
